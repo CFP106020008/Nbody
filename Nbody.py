677f2d571880d6d9d71b755477d6be940be537e4
Lin Yen-Hsing <julius52700@gapp.nthu.edu.tw>
2021-12-09 23:25:55 +0800

    now it worsk

--- Nbody.py
+++ Nbody.py
@@ -5,16 +5,17 @@ from constants import *
 import init
 
 # Let's use MKS
-N = int(1e1) # Number of Shells
-T = 86400*500
+N = int(10) # Number of Shells
+T = 86400*365*500
 
 D = np.zeros((7, N))
+
+
 D = init.Circular_motion(D, 1.5e11, 6e24)
 
 def Acc(D):
     a = np.zeros((3, N))
     for i in range(N):
-        a_ij = np.ones(N)
         M = D[6,:]
         r_ij = D[0:3,:] - np.outer(D[0:3,i], np.ones(N))
         r_ij[:,i] = 1 # To avoid divided by 0
@@ -28,12 +29,8 @@ def function(t, y):
     y = y[:,-1].reshape(7,N)
     r = y[0:3,:]
     v = y[3:6,:]
-    #D[0:3,:] = r
-    #D[3:6,:] = v
     M = D[6,:]
-    dfdt = np.row_stack((np.concatenate((v, Acc(y)), axis=0),M)).flatten()
-    #dxdt = v
-    #dvdt = Acc(D)
+    dfdt = np.row_stack((np.concatenate((v, Acc(y)), axis=0),np.zeros(N))).flatten()
     return dfdt
 
 sol = solve_ivp(fun = function,
@@ -47,7 +44,6 @@ print(np.shape(sol.y))
 SOL = np.ones((7, N, np.shape(sol.y)[1]))
 for i in range(np.shape(sol.y)[1]):
     SOL[:,:,i] = sol.y[:,i].reshape((7,N))
-#SOL.reshape((7, 10, np.shape(sol.y)[1]))
 for i in range(N):
     plt.plot(SOL[0,i,:], SOL[1,i,:])
 #plt.ylim([0,1])
--- init.py
+++ init.py
@@ -3,7 +3,7 @@ import constants as c
 
 def Circular_motion(D, R, m):
     N = np.shape(D)[1]
-    M = N*m 
+    M = N*m/2
     V = (c.G*M/R)**0.5
     for i in range(N):
         D[0:3,i] = np.array([R*np.cos(2*np.pi*i/N), R*np.sin(2*np.pi*i/N), 0])
@@ -11,3 +11,13 @@ def Circular_motion(D, R, m):
     D[6,:] = m
     return D
 
+def Sun_Earth(D): 
+    # Sun
+    D[:3,0] = 0
+    D[3:6,0] = 0
+    D[6,0] = 2e30
+    # Earth
+    D[:3,1] = np.array([1.5e11, 0, 0])
+    D[3:6,1] = np.array([0, 2.4e4, 0])
+    D[6,1] = 6e24
+    return D
